ac9bbd43d4403859de4b0220b970492228efdcef
Kevin James <jesmjamind11@gmail.com>
2025-01-30 23:28:23 +0530

    Add files via upload

---
+++ Quizzler_App/Flight_Deals_Project/main.py
@@ -0,0 +1,5 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+sheety_files = DataManager()
+sheety_files.get_sheety_details()
