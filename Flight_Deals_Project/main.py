6b12979ce34a88e31825d20c01c5e762c59a0cde
Kevin James <jesmjamind11@gmail.com>
2025-02-02 23:34:24 +0530

    Delete Flight_Deals_Project/main.py

--- Flight_Deals_Project/main.py
+++
@@ -1,5 +0,0 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-from data_manager import DataManager
-
-sheety_files = DataManager()
-sheety_files.get_sheety_details()
